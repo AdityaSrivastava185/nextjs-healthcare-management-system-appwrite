9ce09e881d517c586ecbc0b96a6d90b81510ace9
Aditya-Srivastava <pks85715@gmail.com>
2024-10-23 00:50:17 +0530

    files updated

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -101,6 +101,7 @@ export const AppointmentForm = ({
             cancellationReason: values.cancellationReason,
           },
           type,
+          timeZone: 'UTC', // Add the timeZone property here
         };
 
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
